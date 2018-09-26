6f823f95eaea2eb4f9cc9b723cc397e43ab0c684
pierre donzel <p.donzel-ext@lefebvre-sarrut.eu>
2018-09-26 09:01:30 +0200

    maj map

--- js/basics.js
+++ js/basics.js
@@ -3,41 +3,17 @@ function combo(){
   
   mainText = main.textContent;
   markedText = main.children[0].textContent;
-  modulationText = document.getElementById("modulation").textContent;
-   
-  console.log(mainText);
-  console.log(markedText);
-  console.log(modulationText);
+  console.log(document.querySelector('input[name = "query"]:checked').value);
+  modulationText = document.querySelector('input[name = "query"]:checked').value;
   
   result = mainText.replace(markedText, modulationText);
-  console.log(result);
   
-var copyText = document.querySelector("#input");
+  
+  var copyText = document.querySelector("#input");
   document.getElementById("input").value  = result;
   copyText.select();
   document.execCommand("Copy");
   
-  //copyToClipboard(result);
+
  
 }
-
-function copyToClipboard(stringToPaste) {
-	/*document.getElementById("copyTextArea").value = "";*/
-	document.getElementById("copyTextArea").textContent = stringToPaste;	
-	document.getElementById("copyTextArea").select;
-	document.execCommand('copy') ;	
-	
-}
-
-function addToInsideConsole(text) {
-  // crée un nouvel élément div
-  // et lui donne un peu de contenu
-  const nouveauDiv = document.createElement("div");
-  const nouveauContenu = document.createTextNode(text);
-  nouveauDiv.appendChild(nouveauContenu) //ajoute le contenu au div
-  
-  // ajoute l'élément qui vient d'être créé et son contenu au DOM
-  const divActuel = document.getElementById("console");
-  document.body.insertBefore(nouveauDiv, divActuel);
-}
-
